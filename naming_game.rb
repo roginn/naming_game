c950685e6ba22084bf9ff2a6012d0239bd3c69f6
Roger Garcia <roger@redealumni.com>
2015-06-30 00:46:34 -0300

    first version

--- naming_game.rb
+++ naming_game.rb
@@ -1,5 +1,10 @@
+require 'set'
+
 class Word
   @@count = 0
+  def self.count
+    @@count
+  end
 
   def self.get_new_one
     @@count += 1
@@ -16,18 +21,98 @@ class Node
 end
 
 class Player
-  attr_reader words
+  attr_accessor :words
 
-  def speak_to(hearer)
-    intersection = self.words & hearer.words
-    if intersection.empty?
-      
+  def initialize
+    @words = []
+  end
+
+  def pick_random_word
+    if @words.empty?
+      @words << Word.get_new_one
+    end
+
+    @words.sample
+  end
+
+  def has_word?(w)
+    @words.include? w
+  end
+
+  def drop_all_but(w)
+    @words = [w]
+  end
+
+  def add_word(w)
+    @words << w
+  end
+
+  def speak_to(listener)
+    w = self.pick_random_word
+    if listener.has_word? w
+      # success
+      self.drop_all_but w
+      listener.drop_all_but w
     else
-      word = intersection.sample
-      self.keep_only word
-      hearer.keep_only word
+      # failure
+      listener.add_word w
     end
   end
 end
 
+class Game
+  attr_accessor :players, :iterations
 
+  class << self
+    def run(n = 100)
+      game = Game.new n
+      until game.stop_condition?
+        game.iterate
+      end
+      game.report_results
+    end
+  end
+
+  def initialize(n = 100)
+    @players    = []
+    @iterations = 0
+    @max_words  = 0
+    @time_to_max_words = 0
+
+    (1..n).each do |i|
+      @players << Player.new
+    end
+  end
+
+  def all_words
+    @players.map { |x| x.words }.flatten.to_set
+  end
+
+  def stop_condition?
+    @players.map { |x| x.words.size == 1 }.all? and self.all_words.size == 1
+    # @players.map { |x| x.words.size == 1 }.all? and @num_words == 1
+  end
+
+  def iterate
+    p = @players.sample
+    q = (@players - [p]).sample
+    p.speak_to q
+
+    @iterations += 1
+    num_words = self.all_words.size
+    if num_words > @max_words
+      @max_words = num_words
+      @time_to_max_words = @iterations
+    end
+  end
+
+  def report_results
+    {
+      words:       Word.count,
+      iterations:  @iterations,
+      convergence: self.all_words.to_a.first
+    }
+  end
+end
+
+Benchmark.bm { |x| x.report { Game.run 1000 } }
