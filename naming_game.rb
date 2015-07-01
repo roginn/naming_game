22c12a6774b651ac26d442072f4d9b189df1a6cb
Roger Garcia <roger@redealumni.com>
2015-06-30 21:25:04 -0300

    adds experiment method

--- naming_game.rb
+++ naming_game.rb
@@ -150,3 +150,15 @@ end
 
 # game = Game.new; game.run
 Benchmark.bm { |x| x.report { game = Game.new; puts game.run } }
+
+
+def experiment
+  puts "network_size\titerations\tmax_words\titerations\tconvergence\ttime_to_max_words"
+  (100..2000).step(100).each do |network_size|
+    (1..30).each do |iteration|
+      game = Game.new network_size;
+      result = game.run;
+      puts "#{network_size}\t#{iteration}\t#{result[:max_words]}\t#{result[:iterations]}\t#{result[:convergence].id}\t#{result[:time_to_max_words]}"
+    end
+  end
+end
